ea2f5b2f7083e7ad78b831fe1e02d97e8e6b5cf1
Tanishq Mishra <72483807+tanishqmishra7@users.noreply.github.com>
2023-12-18 09:18:13 +0530

    Update main.go

--- main.go
+++ main.go
@@ -193,17 +193,6 @@ func deleteTodo(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	app := gofr.New()
-
-    // register route greet
-    app.GET("/greet", func(ctx *gofr.Context) (interface{}, error) {
-
-        return "Hello World!", nil
-    })
-
-    // Starts the server, it will listen on the default port 8000.
-    // it can be over-ridden through configs
-    app.Start()
 
 
 	stopChan := make(chan os.Signal)
